170557679f1c4f9b363db1416dff2ba1d8240d40
WarGooglee <s00227303@mail.itsligo.ie>
2023-09-24 23:46:49 +0100

    Task 3

--- index.js
+++ index.js
@@ -8,4 +8,34 @@ app.get('/bananas', (req, res) => res.send('hello world, this is bananas'));
 app.get('/bananas', (req, res) => res.send('hello world, this is bananas'));
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var contacts = [];
+
+app.use(express.json());
+
+app.post('/contacts', (req, res) => {
+    const contact = req.body;
+  
+
+    console.log(contact) // to check what was received
+   
+    contacts.push(contact);
+
+    res.send ('contact has been added to the database');
+    console.log(`contact name is ${contact.name} number of contacts is ${contacts.length}`);
+
+});
+
+app.get('/contacts', (req, res) => {
+    res.send(contacts);
+})
+
+app.get('/contacts/:id', (req,res) => {
+
+  let id = req.params.id; 
+  res.json(contacts[id]);
+})
+
+
+
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
